c1531214071387bc24c7c8319604f06059d4cccb
FC333 <2990299255@qq.com>
2022-09-20 18:28:07 +0800

    f commit

--- ch04-add/src/com/zkx/A.java
+++ ch04-add/src/com/zkx/A.java
@@ -5,7 +5,6 @@ package com.zkx;
  */
 public class A {
     public static void main(String[] args) {
-        System.out.println("1");
-         System.out.println("2222a");
+        System.out.println("f");
     }
 }
