690a936090772bd179ba78da30b42dfaf05d278d
FC333 <2990299255@qq.com>
2022-09-20 18:15:33 +0800

    A222 commit

--- ch04-add/src/com/zkx/A.java
+++ ch04-add/src/com/zkx/A.java
@@ -6,6 +6,6 @@ package com.zkx;
 public class A {
     public static void main(String[] args) {
         System.out.println("1");
-        System.out.println("22");
+        System.out.println("222");
     }
 }
